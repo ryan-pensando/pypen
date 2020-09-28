a61ec2c1ba71493b1a13053a2d59ac2f1e1089a5
ryan <ryan@pensando.io>
2020-09-28 12:24:10 -0500

    init

--- pen_fw_2_kafka.py
+++ pen_fw_2_kafka.py
@@ -1,3 +1,19 @@
+# Copyright (c) 2020, Pensando Systems
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+# Author: Ryan Tischer ryan@pensando.io
+
+
 from kafka import KafkaProducer
 from io import BytesIO
 import pen, gzip, json
@@ -24,3 +40,8 @@ def dsc_metrics_kfk(PSM_IP,session,PSM_TENANT, dsc_list, startTime, endTime):
     for int in dsc_list:
         data = pen.get_dsc_metrics(PSM_IP, session, PSM_TENANT, int, startTime, endTime)
         producer.send(kafkaTopic, json.dumps(data).encode('utf-8'))
+
+def send_2_kfk(data):
+
+    producer = KafkaProducer(bootstrap_servers=kafkaServer)
+    producer.send(kafkaTopic, json.dumps(data).encode('utf-8'))
